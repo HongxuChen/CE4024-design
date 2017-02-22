d4f3e10e219dc206c08c7171a6b93eb3a77d2fe4
Hongxu <hongxuchen1989@gmail.com>
2017-02-22 12:52:50 +0800

    added faq

--- STUDENTS/src/Attacker1.java
+++ STUDENTS/src/Attacker1.java
@@ -4,8 +4,6 @@ import common.Config;
 import static common.Utils.*;
 
 public class Attacker1 {
-	
-	// you are free to ADD fields, methods and implements, however DON'T REMOVE any
 
     final private Oracle1 oracle;
 
@@ -21,21 +19,21 @@ public class Attacker1 {
     // you are allowed to add extra *private* member functions
     // however they won't be exposed to the unit tests
     // you are not allowed to add any fields
-    
-    /// --- start of implementation area    
+
+    /// --- start of implementation area
 
     public byte[] decryptSuffix() {
     	// TODO implement decryption code here
     	return null;
     }
-    
+
     /// --- end of implementation area
     ////////////////////////////////////////////////////////////////////////////////
-    
+
     // sample test input
     // note: we internally use a JUnit like tool so this "main" function only serves as
     // a demo to show what the unit test will be like
-    
+
     // we will use more inputs to test your implementation
     // in the unit tests, by
     // * changing Oracle1's key
@@ -43,7 +41,7 @@ public class Attacker1 {
     public static void main(String[] args) {
         ///////////////////////////////////////////////////////////
         String key = "3%ac^`+=";  // a different key
-        String suffix = "a19q-j*"; // a different suffix
+        String suffix = "a(19q-j*"; // a different suffix
         ///////////////////////////////////////////////////////////
         Oracle1 oracle = new Oracle1(key.getBytes(), suffix.getBytes());
         Attacker1 attacker = new Attacker1(oracle);

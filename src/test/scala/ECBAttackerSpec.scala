2826b7e4e84798594883721535abeef749fcfb03
Hongxu <hongxuchen1989@gmail.com>
2017-04-11 11:45:44 +0800

    simplify tests

--- src/test/scala/ECBAttackerSpec.scala
+++ src/test/scala/ECBAttackerSpec.scala
@@ -1,4 +1,5 @@
 import common.{Config, CryptoSpec, Utils}
+import org.bouncycastle.pqc.math.linearalgebra.ByteUtils
 import org.scalatest.time.{Millis, Span}
 
 class ECBAttackerSpec extends CryptoSpec {
@@ -13,18 +14,9 @@ class ECBAttackerSpec extends CryptoSpec {
   }
 
   // computed suffix's length may not be fixed
-  "shorter suffix" should "be same for first n bytes" in {
-    //    val key1 = "abcdefghijklmnop"
-    val key1 = "abcdefgh"
-    val suffix1 = "123456"
-    val attacker1 = new Attacker1(new Oracle1(key1, suffix1))
-    val computed1 = attacker1.decryptSuffix()
-    println(Utils.ppBytes(computed1))
-    computed1.slice(0, suffix1.length) should contain theSameElementsAs suffix1.getBytes()
-    //////////////////////////////////////////////////////
-    //    val key2 = Array[Byte](-23, 4, 9, 98, -128, -77, 9, 7, 33, 127, 32, 45, 99, 4, 37, 24)
-    val key2 = Array[Byte](-23, 4, 9, 98, -128, -77, 9, 7)
-    val suffix2 = "!>!A@$`".getBytes()
+  "strange suffix" should "pass" in {
+    val key2 = ByteUtils.fromHexString("e904096280b30907")
+    val suffix2 = "!>*A-@$`".getBytes()
     val attacker2 = new Attacker1(new Oracle1(key2, suffix2))
     val computed2 = attacker2.decryptSuffix()
     println(Utils.ppBytes(computed2))
@@ -34,8 +26,8 @@ class ECBAttackerSpec extends CryptoSpec {
   "byte suffix" should "be decrypted" in {
     //    val key = Array[Byte](-23, 4, 9, 98, -128, -0x42, 9, 7, 33, 0x7f, 0x32, 45, -99, 4, 0x35, 24)
     //    val suffix = Array[Byte](32, 99, -45, 78, 0, -87, 38, 98, 73, 92, 43, 0, 0, -22, -54, -93)
-    val key = Array[Byte](-23, 4, 9, 98, -128, -0x42, 9, 7)
-    val suffix = Array[Byte](32, 99, -45, 78, 0, -87, -38, 98)
+    val key = ByteUtils.fromHexString("e904096280b30907")
+    val suffix = ByteUtils.fromHexString("2063d34e00a9da62")
     val attacker = new Attacker1(new Oracle1(key, suffix))
     val computed = attacker.decryptSuffix()
     println(Utils.ppBytes(computed))
